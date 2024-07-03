fd43e2c9bce0e0faf9036193f0ef670e42138863
Alex <fenix_al@tut.by>
2024-07-03 10:36:00 +0300

    Sql Driver

--- src/main/java/by/alex/spring/config/SpringConfig.java
+++ src/main/java/by/alex/spring/config/SpringConfig.java
@@ -5,12 +5,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -39,6 +42,12 @@ public class SpringConfig implements WebMvcConfigurer{
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        return null;
     }
 
     @Override
